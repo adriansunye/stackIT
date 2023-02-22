08dbf6eed118408bd78018966be4851c90de7888
rcarabal2022 <raquelcaraballospain@gmail.com>
2023-02-22 20:49:58 +0100

    header in progress

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from '@/components/navigation/header/Header' 
 
 const Layout = ({ children }) => {
     return (
         <React.Fragment>
             <Outlet />
+            <Header />
             {children}
-        </React.Fragment>
+            </React.Fragment>
     )
 }
 
